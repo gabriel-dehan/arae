0159df75272ef0b3bf952970718ccd93c00a0667
gabriel-dehan <dehan.gabriel@gmail.com>
2012-04-21 16:33:59 +0200

    Bit of documentation

--- client/libs/_string.js
+++ client/libs/_string.js
@@ -1,8 +1,17 @@
+/**
+ * Trim a character from the begining and the end of a string
+ * @param [Object String] c Character to be removed
+ * @return [Object String] Trimmed string
+ */
 String.prototype.trim = function(c) {
     var r = new RegExp('(^' + c + ')|(' + c + '$)', 'g');
     return this.replace(r, "");
 }
 
+/**
+ * Uppercase the first character of a string
+ * @return [Object String] Capitalized string
+ */
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
