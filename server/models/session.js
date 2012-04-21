a79e0654f0b0ea1eb55091d816b7f6f08e0f0ee1
gabriel-dehan <dehan.gabriel@gmail.com>
2012-04-21 16:11:14 +0200

    The server session is shared between all clients, beware.

--- server/models/session.js
+++ server/models/session.js
@@ -1,4 +1,4 @@
 /**
- * We define a collection which represent a permanent session through every state of the application
+ * We define a collection which represent a permanent session through every state of the application for every client
  */
 var _Session = new Meteor.Collection('server_session');
