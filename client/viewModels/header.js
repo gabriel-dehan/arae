79af5dfd16d942bd757e0d301641a07940a2041d
gabriel-dehan <dehan.gabriel@gmail.com>
2012-04-22 21:50:32 +0200

    Logout

---
+++ client/viewModels/header.js
@@ -0,0 +1,24 @@
+/**
+ * Checks if the user is logged in
+ * @return {boolean}
+ */
+Template.header.is_logged = function() {
+    return Session.get('user') !== undefined;
+}
+
+/**
+ * Get the logged user name
+ * @return {string} User name
+ */
+Template.header.user_name = function() {
+    return Session.get('user').name;
+}
+
+/* Set the logout click event */
+Template.header.events = {
+    'click .logout_link': function(e) {
+        /* TODO: Set a message */
+        Session.set('user', undefined);
+        Meteor.navigate('/user/list');
+    }
+}
