150d69cd5a6690b2e27e4ff715f85f73be263d5b
gabriel-dehan <dehan.gabriel@gmail.com>
2012-04-27 01:04:17 +0200

    Improved String.trim([c])

--- libs/_string.js
+++ libs/_string.js
@@ -1,12 +1,16 @@
 /**
- * Trim a character from the begining and the end of a string
- * @param [Object String] c Character to be removed
+ * Trim all whitespaces OR a character (if specified) from the begining and the end of a string
+ * @param [Object String] c Character to be removed (optional)
  * @return [Object String] Trimmed string
  */
-String.prototype.trim = function(c) {
-    var r = new RegExp('(^' + c + ')|(' + c + '$)', 'g');
-    return this.replace(r, "");
-}
+String.prototype.trim = function (c) {
+    if (c) {
+        var r = new RegExp('(^' + c + ')|(' + c + '$)', 'g');
+        return this.replace(r, '');
+    } else {
+        return this.replace(/^\s+|\s+$/g,'');
+    }
+};
 
 /**
  * Uppercase the first character of a string
@@ -14,7 +18,7 @@ String.prototype.trim = function(c) {
  */
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
-}
+};
 
 /**
  * Add the string to itself n times
