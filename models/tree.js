89c270721907103e7aada69fbf79784557f25428
gabriel-dehan <dehan.gabriel@gmail.com>
2012-04-29 21:23:58 +0200

    Not creating any user or document tree on server startup, except User#admin

--- models/tree.js
+++ models/tree.js
@@ -3,60 +3,6 @@ var DocumentTree = new Meteor.Collection('tree');
 
 if ( Meteor.is_server ) {
     Meteor.startup(function() {
-
-//        tc = DocumentTree.findOne();
-//        t = new Tree(tc.root)
-//        console.log(t.tree);
-//        t.move(6, 1);
-//        console.log(t.tree);
-
 //        DocumentTree.remove({});
-        if( DocumentTree.find().count() === 0 ) {
-            var t = [
-                {
-                    is_dir : 0,
-                    name   : 'file.txt',
-                    _id    : 0
-                },
-                {
-                    is_dir  : 1,
-                    name    : 'foo',
-                    _id     : 1,
-                    tree    : [
-                        {
-                            is_dir : 0,
-                            _id    : 2,
-                            name   : 'leaf.txt'
-                        }
-                    ]
-                },
-                {
-                    is_dir  : 1,
-                    name    : 'bar',
-                    _id     : 3,
-                    tree    : [
-                        {
-                            is_dir: 0,
-                            _id     : 4,
-                            name   : 'leaf1.txt'
-                        },
-                        {
-                            is_dir: 0,
-                            _id     : 5,
-                            name   : 'leaf2.txt'
-                        },
-                        {
-                            is_dir  : 1,
-                            name    : 'bar',
-                            _id     : 6,
-                            tree    : []
-                        }
-                    ]
-                }
-            ];
-
-            DocumentTree.insert({root: t, count:7});
-            console.log(DocumentTree.findOne());
-        }
     });
 }
--- models/user.js
+++ models/user.js
@@ -2,35 +2,9 @@ User = new Meteor.Collection("user");
 
 if ( Meteor.is_server ) {
     Meteor.startup(function() {
-
 //        User.remove({});
-        if( User.find().count() === 0 ) {
-            var users = [
-                {
-                    name : 'admin',
-                    pass : 'password',
-                    tree_id : DocumentTree.findOne()._id
-                },
-                {
-                    name : 'nicolas',
-                    pass : 'nicolas',
-                    tree_id : DocumentTree.findOne()._id
-                },
-                {
-                    name : 'gabriel',
-                    pass : 'gabriel',
-                    tree_id : DocumentTree.findOne()._id
-                }
-            ];
-            _.each(users, function(user) {
-                User.insert(
-                    {
-                        name : user.name,
-                        pass : md5(user.pass),
-                        tree_id : user.tree_id
-                    }
-                );
-            });
-        }
+       if ( User.find().count() === 0 ) {
+           User.insert({name : 'admin', pass : md5('password')});
+       }
     });
 }
